63d729e1e786880531203a994c0fc80eda5fa0bf
lelekaspb <bov-1986@yandex.ru>
2021-09-07 18:56:51 +0200

    optimized the code with more functions

--- script.js
+++ script.js
@@ -2,13 +2,23 @@
 
 const colorPicker = document.querySelector("#colorId");
 
-colorPicker.addEventListener("input", displayChanges);
+colorPicker.addEventListener("input", getUserInput);
 
-function displayChanges(e) {
-  updateOutput(e.target.value);
-  updateHex(e.target.value);
-  const rgb = updateRgb(e.target.value);
-  updateHsl(rgb);
+function getUserInput(e) {
+  const hexColor = e.target.value;
+  displayChanges(hexColor);
+}
+
+function displayChanges(hexColor) {
+  const rgb = hexToRgb(hexColor);
+  const rgbColor = rgbToCss(rgb);
+  const hsl = rgbToHsl(rgb);
+  const hex = rgbToHex(rgb);
+
+  updateOutput(rgbColor);
+  updateHex(hex);
+  updateHsl(hsl);
+  updateRgb(rgb);
 }
 
 function updateOutput(color) {
@@ -19,23 +29,39 @@ function updateHex(hex) {
   document.querySelector(".hex > span").textContent = hex;
 }
 
-function updateRgb(hex) {
-  let rgb = {};
+function updateRgb(rgbObject) {
+  document.querySelector(
+    ".rgb > span"
+  ).textContent = `${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b}`;
+}
+
+function updateHsl(hsl) {
+  document.querySelector(
+    ".hsl > span"
+  ).textContent = `${hsl.h}, ${hsl.s}, ${hsl.l}`;
+}
+
+// hex to rgb function
+function hexToRgb(hex) {
   const hexStr = hex.slice(1);
   const red = parseInt(hexStr.substring(0, 2), 16);
   const green = parseInt(hexStr.substring(2, 4), 16);
   const blue = parseInt(hexStr.substring(4, 6), 16);
-  document.querySelector(
-    ".rgb > span"
-  ).textContent = `${red}, ${green}, ${blue}`;
-  return (rgb = {
+
+  return {
     r: red,
     g: green,
     b: blue,
-  });
+  };
 }
 
-function updateHsl(rgbObject) {
+// rgb to css string function
+function rgbToCss(rgbObject) {
+  return `rgb(${rgbObject.r}, ${rgbObject.g}, ${rgbObject.b})`;
+}
+
+// rgb to hsl function
+function rgbToHsl(rgbObject) {
   let r = rgbObject.r;
   let g = rgbObject.g;
   let b = rgbObject.b;
@@ -76,7 +102,48 @@ function updateHsl(rgbObject) {
 
   //console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
 
-  document.querySelector(".hsl > span").textContent = `${h.toFixed(
-    0
-  )}, ${s.toFixed(0)}%, ${l.toFixed(0)}%`;
+  return {
+    h: `${h.toFixed(0)}`,
+    s: `${s.toFixed(0)}%`,
+    l: `${l.toFixed(0)}%`,
+  };
+}
+
+function rgbToHex(rgbObject) {
+  let hex = "#";
+
+  hex += getBasicHexColor(rgbObject.r);
+  hex += getBasicHexColor(rgbObject.g);
+  hex += getBasicHexColor(rgbObject.b);
+
+  return hex;
+}
+
+// get two hex numbers for every color (reg, green, and blue)
+function getBasicHexColor(rgbNumber) {
+  let twoOfHex = "";
+  const char1 = Math.floor(rgbNumber / 16);
+  twoOfHex += getHexChar(char1);
+  const char2 = rgbNumber % 16;
+  twoOfHex += getHexChar(char2);
+  return twoOfHex;
+}
+
+// turn hex number to hex character
+function getHexChar(num) {
+  if (num >= 0 && num <= 9) {
+    return num;
+  } else if (num === 10) {
+    return "A";
+  } else if (num === 11) {
+    return "B";
+  } else if (num === 12) {
+    return "C";
+  } else if (num === 13) {
+    return "D";
+  } else if (num === 14) {
+    return "E";
+  } else if (num === 15) {
+    return "F";
+  }
 }
